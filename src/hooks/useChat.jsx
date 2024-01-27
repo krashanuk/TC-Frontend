4007d5727718251a4ccfee553bea9a4f1aafbf9d
krashanuk <62797387+krashanuk@users.noreply.github.com>
2024-01-28 01:49:53 +0530

    Update useChat.jsx

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = "http://89.163.152.181:3000/";
+const backendUrl = "http://89.163.152.181:3000";
 const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
