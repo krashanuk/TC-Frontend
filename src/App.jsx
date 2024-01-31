bc135dbe8ff75e7f390f6e205a190951ee5b5443
krashanuk <62797387+krashanuk@users.noreply.github.com>
2024-01-31 06:47:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <Loader />
-      <Leva hidden/>
-      <UI />
+      <Leva />
+      <UI hidden/>
       <Canvas shadows camera={{ position: [0, 0, 1], fov: 30 }}>
         <Experience />
       </Canvas>
